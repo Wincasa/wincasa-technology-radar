985786a6f1de44aac037aef9f97c8f9828bedcf4
Nicolas Seiler <nicolas.seiler@wincasa.com>
2018-12-17 14:13:49 +0100

    Fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const path = require('path');
 const buildPath = path.join(__dirname, './dist');
 const args = require('yargs').argv;
 
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 let isProd = args.prod;
@@ -21,7 +21,7 @@ if (isDev) {
 }
 
 let plugins = [
-    new ExtractTextPlugin('[name].[hash].css'),
+    new MiniCssExtractPlugin({filename: '[name].[hash].css'}),
     new HtmlWebpackPlugin({
         template: './src/index.html',
         chunks: ['main'],
@@ -68,16 +68,31 @@ module.exports = {
     },
 
     module: {
-        loaders: [
-            { test: /\.json$/, loader: 'json'},
-            { test: /\.js$/, exclude: /node_modules/, loader: 'babel'},
-            { test: /\.scss$/, exclude: /node_modules/, loader: ExtractTextPlugin.extract('style', 'css?sourceMap!sass') },
-            { test: /\.(png|jpg|ico)$/, exclude: /node_modules/, loader: 'file-loader?name=images/[name].[ext]&context=./src/images' }
+        rules: [
+            { test: /\.json$/, use: 'json-loader', exclude: /node_modules/ },
+            { test: /\.js$/, exclude: /node_modules/, use: 'babel-loader', exclude: /node_modules/ },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(png|jpg|ico)$/,
+                exclude: /node_modules/,
+                loader: 'file-loader',
+                exclude: /node_modules/,
+                options: {
+                    name: 'images/[name].[ext]',
+                    context: './src/images'
+                }
+            }
         ]
     },
-
-    quiet: false,
-    noInfo: false,
 
     plugins: plugins,
 
